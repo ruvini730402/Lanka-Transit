aa43ae36679bbac42f39858fbed83a30402808c3
AshanXJay <ashan@anagatha.com>
2025-07-19 15:28:09 +0530

    Index.php
    Signed-off-by: AshanXJay <ashan@anagatha.com>

--- index.php
+++ index.php
@@ -188,7 +188,7 @@
     <footer class="bg-dark text-white py-4 mt-5">
         <div class="container">
             <div class="row">
-                    <p>&copy; 2025 Lanka Transit. All rights reserved.</p>
+                    <p>&copy; Lanka Transit. All rights reserved.</p>
             </div>
         </div>
     </footer>
