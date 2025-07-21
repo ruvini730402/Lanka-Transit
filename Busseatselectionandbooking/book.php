7a7c0655531a2756f19cb875dd110fb2d33b7576
H.H.Ruvini Kanchana <cst22029@std.uwu.ac.lk>
2025-07-21 12:28:05 +0530

    seat selection and booking

---
+++ Busseatselectionandbooking/book.php
@@ -0,0 +1,10 @@
+<?php
+// book.php
+header('Content-Type: application/json');
+require_once 'classes/Booking.php';  // Include the OOP class
+
+// Create and process the booking using POST data
+$booking = new Booking($_POST);
+$booking->processBooking();
+
+
