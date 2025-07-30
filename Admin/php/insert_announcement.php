0be1ecf72b23803114749e92877c2e9750a1807c
Ms. G.D.U. Jayaweera <cst22104@std.uwu.ac.lk>
2025-07-30 20:26:03 +0530

    Add files via upload

---
+++ Admin/php/insert_announcement.php
@@ -0,0 +1,17 @@
+<?php
+include('../dbcon.php');
+include('Announcement.php');
+
+if (isset($_POST['add_announcement'])) {
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+
+    $announcement = new Announcement($connection);
+    if ($announcement->insert($title, $content)) {
+        header("Location: ../announcement_display.php?msg=Announcement added successfully");
+    } else {
+        header("Location: ../announcement_display.php?msg=Failed to add announcement");
+    }
+}
+?>
+
