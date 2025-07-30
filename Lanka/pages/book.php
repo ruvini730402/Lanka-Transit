22ce3eacc256eb2b6e82318fdf8044c962ec9355
H.H.Ruvini Kanchana <cst22029@std.uwu.ac.lk>
2025-07-30 20:54:57 +0530

    Delete Lanka directory

--- Lanka/pages/book.php
+++
@@ -1,9 +0,0 @@
-<?php
-// book.php
-header('Content-Type: application/json');
-require_once 'Booking.php';  // Include the OOP class
-
-// Create and process the booking using POST data
-$booking = new Booking($_POST);
-$booking->processBooking();
-
